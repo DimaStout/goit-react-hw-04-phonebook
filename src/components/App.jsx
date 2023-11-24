a5ac645da02882f3f4b208857a157491de6d3c15
Dmytro Zinchuk <127327533+DimaStout@users.noreply.github.com>
2023-11-24 20:28:58 +0100

    goit-react-hw-04-phonebook-002

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,22 +7,20 @@ import { Section } from './App.styled';
 
 const storageKey = 'contacts';
 
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem(storageKey);
+    return savedContacts ? JSON.parse(savedContacts) : initialState;
+  });
 
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const savedContacts = localStorage.getItem(storageKey);
-    if (savedContacts !== null) {
-      setContacts(JSON.parse(savedContacts));
-    }
-  }, []);
 
   useEffect(() => {
     localStorage.setItem(storageKey, JSON.stringify(contacts));
